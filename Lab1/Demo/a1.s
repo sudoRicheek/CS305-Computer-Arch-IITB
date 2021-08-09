e927e69d6ff38ea9bf883e242230d02b722dfab5
Richeek Das <60005701+sudoRicheek@users.noreply.github.com>
2021-08-09 22:40:35 +0530

    lab1 done

--- Lab1/Demo/a1.s
+++ Lab1/Demo/a1.s
@@ -16,6 +16,7 @@
         li $v0 5            # read int
         syscall
         move $s0 $v0        # $s0 has 'n' [IMPORTANT]
+        beq $s0 $zero pz_m  # if n==0 branch to print zero max zig-zag length
 
         sll $a0 $v0 2       # number of bytes allocated now in $a0=$v0<<2
         li $v0 9            # sbrk heap allocation op code
@@ -40,21 +41,60 @@
         
         bne $t0 $s0 loop_input
 
+
+    # Section to calculate max ZigZag length
+    zz_init:
+        move $t0 $zero      # lastSign in $t0
+        li $t1 1            # length of zigzag in $t1
+        li $t2 1            # i of the loop_mzz in $t2
+
+        move $t3 $s1        # base address of the array, i.e. array[0] in $t3
+        addi $t4 $s1 4      # address of array[1] in $t4
     
-        move $t0 $zero
-        move $t1 $s1
+    loop_mzz:               # loop for calculating the max zig-zag length
+        beq $t2 $s0 p_m
 
-    ; loop_print:
-    ;     li $v0 1
-    ;     lw $a0 ($t1)
-    ;     syscall
+        lw $t5 ($t3)        # array[i-1]
+        lw $t6 ($t4)        # array[i]
 
-    ;     addi $t0 $t0 1
-    ;     addi $t1 $t1 4
+            slt $t7 $t5 $t6 # $t7=1 if array[i-1]<array[i]
+            beq $t7 $0 elif # if array[i-1]>=array[i] goto [elif]
+            j enif          # if $t7==1 sign=1
+        elif:
+            slt $t7 $t6 $t5 # $t7=1 if array[i-1]>array[i] --> if $t7==0 sign=0 
+            beq $t7 $0 enif # if array[i-1]==array[i] goto [enif]
+            li $t7 2        # if array[i-1]>array[i] --> sign=2 [-ve] 
+            j enif              
+        enif:
+                                    # $t7 contains the sign [IMPORTANT]
+                                    # 0 -> equal, 1-> +ve, 2-> -ve
+        
+            bne $t7 $t0 s_neq_ls    # $t0 has the ls
+            j j_loop_mzz
+        s_neq_ls:
+            beq $t7 $0 j_loop_mzz   # if s neq ls and s neq 0
+            move $t0 $t7            # ls = s
+            addi $t1 $t1 1          # length++
+            j j_loop_mzz
+        j_loop_mzz:
+            addi $t2 $t2 1          # increment i
+            move $t3 $t4            # point to array[i-1]
+            addi $t4 $t4 4          # point to array[i]
+            j loop_mzz
 
-    ;     bne $t0 $s0 loop_print
+    p_m:                    # print length of max zigzag
+        move $s2 $t1        # $s2 stores the length of maxZigZag
+        li $v0 1
+        move $a0 $s2
+        syscall                     
+        j exit
 
-    
+    pz_m:                   # print length of max zigzag is zero
+        li $v0 1
+        move $a0 $zero
+        syscall
+        j exit
+
     exit:
         li $v0 10           # load exit opcode
         syscall
